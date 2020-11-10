b7a022da34820ade6bac3b9939c38848b5965437
HenokZeg <henokzegeye1990@gmail.com>
2020-11-10 23:47:03 +0300

    User controller added

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :items
   resources :carts
   resources :cart_items
+  resources :users
   post '/bulk_create', controller: :cart_items, action: :bulk_create
 end
