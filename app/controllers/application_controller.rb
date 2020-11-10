ceeeafede82fbb675c4b21d1771d5ee94dc622ff
HenokZeg <henokzegeye1990@gmail.com>
2020-11-10 23:51:26 +0300

    Unauthorized message throw if user not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,34 @@
 class ApplicationController < ActionController::API
+    before_action :authenticate
+
+    def logged_in?
+        !current_user.nil?
+    end
+
+    def current_user
+        return unless auth_present?
+
+        user = User.find(auth['id'])
+        @current_user ||= user if user
+    end
+
+    private
+
+      def authenticate
+        render json: { error: 'Unauthorized' }, status: 401 unless logged_in?
+      end
+
+      def token
+        return nil if request.env['HTTP_AUTHORIZATION'].nil?
+
+        request.env['HTTP_AUTHORIZATION'].scan(/Bearer (.*)$/).flatten.last
+      end
+    
+      def auth
+        TokenAuthService.decode(token)
+      end
+
+      def auth_present?
+        !token.nil?
+      end
 end
